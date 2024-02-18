fbb9ce48a40b857ca33206a34ac3f17898548a70
Artem Musalitin <musalitin.a.a@gmail.com>
2024-02-18 11:55:24 +0100

    Add config

--- src/bin/projector.rs
+++ src/bin/projector.rs
@@ -1,7 +1,10 @@
+use anyhow::{Ok, Result};
 use clap::Parser;
-use projector_rust::opts::Opts;
+use projector_rust::{opts::Opts, config::Config};
 
-fn main() {
-    let opts = Opts::parse();
-    println!("{:?}", opts)
+fn main() -> Result<()> {
+    let opts: Config = Opts::parse().try_into()?;
+    println!("{:?}", opts);
+
+    Ok(())
 }
